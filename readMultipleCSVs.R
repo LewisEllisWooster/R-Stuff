3ce7f991865944f09a3b2dd4dc8c97e6061c07ca
LewisEllisWooster <he_wooster@ingham.org>
2015-10-30 13:57:13 -0400

    Create readMultipleCSVs.R

---
+++ readMultipleCSVs.R
@@ -0,0 +1,21 @@
+getmonitor <- function(id, directory, summarize = FALSE) {
+ID<-id
+directory<-read.csv("STRING/ID().csv")
+summarize<-if(summarize==FALSE)
+print(getmonitor)
+else 
+print(summary(getmonitor))}
+"%s/%03s.csv",directory,id
+03d
+
+#sum<-paste(as.numeric(summarize))
+if summarize==TRUE
+{print summary(summary(data))}
+return(data) 
+}
+
+
+id<-as.integer(id)
+filename<-sprintf("%s/%03d.csv",directory,id)
+data<-read.csv(filename)
+return(data)
