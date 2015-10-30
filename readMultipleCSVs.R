9b9c411fa523a343bdab3ea8172f5a2582208456
LewisEllisWooster <he_wooster@ingham.org>
2015-10-30 13:59:33 -0400

    Update readMultipleCSVs.R

--- readMultipleCSVs.R
+++ readMultipleCSVs.R
@@ -5,3 +5,18 @@ data<-read.csv(filename)
 if (summarize==TRUE)
 {print(summary(data))}
 return(data)}
+
+
+excludeSpecificCSV <- function(directory, id = 1:332) {
+nobs<-numeric(0)
+source("getmonitor.R")
+for (id2 in id) {
+data <-  getmonitor(id2,directory)
+nobs <-c(nobs, nrow(na.omit(data)))}
+df <- data.frame(id,nobs)
+return(df)
+}
+
+
+
+
